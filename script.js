1d94e718da02bd1d74fe35e9f3392022b16ff5a9
marco <marcoadrianzambra@gmail.com>
2025-04-13 15:37:52 -0500

    login structure

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.getElementById("loginForm")
+
+    loginForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+    
+    })
+})
